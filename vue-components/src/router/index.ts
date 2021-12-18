3bcd7b50369d26a9739a39e5d76ef521ee32e706
jaylene <L592965@163.con>
2021-12-18 14:50:39 +0800

    更改

--- vue-components/src/router/index.ts
+++ vue-components/src/router/index.ts
@@ -9,6 +9,15 @@ const routes: Array<RouteRecordRaw> = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "J-test" */ '../views/J-test/index.vue')
+  },
+  {
+    path: '/list',
+    name: 'List',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "J-list" */ '../views/J-list/index.vue')
   }
 ]
 
--- vue-components/src/views/J-test/index.vue
+++ vue-components/src/views/J-test/index.vue
@@ -1,17 +1,33 @@
 <template>
-  <el-row>
+  <!-- <el-row>
     <el-button>默认按钮</el-button>
     <el-button type="primary">主要按钮</el-button>
     <el-button type="success">成功按钮</el-button>
     <el-button type="info">信息按钮</el-button>
     <el-button type="warning">警告按钮</el-button>
     <el-button type="danger">危险按钮</el-button>
-  </el-row>
+  </el-row> -->
+  <VirtualList :listData="data" :itemSize="100" />
 </template>
 <script lang="ts">
+import VirtualList from '../J-list/index.vue'
 import { defineComponent } from 'vue'
-
 export default defineComponent({
   // setup () {},
+  components: { VirtualList },
+  data () {
+    const d = []
+    for (let i = 0; i < 100000; i++) {
+      d.push({ id: i, value: i })
+    }
+    return {
+      data: d
+    }
+  }
 })
 </script>
+<style scoped>
+.infinite-list-container {
+  height: 900px;
+}
+</style>
